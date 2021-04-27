72cf1b4173d12b982d9c7b0537a4431765696206
Leon Markovich <leonmarkovich97@gmail.com>
2021-04-27 23:14:37 +0300

    git ignore, added new file opening and some comments

--- converter.py
+++ converter.py
@@ -1,36 +1,47 @@
+import os
+import sys
+import csv
 def insertIntoExists(section):
-	return section.find("INSERT INTO") != -1
+    return section.find("INSERT INTO") != -1
 
 def getTitles(section):
-	i = 0
-	temp = []
-	while section[i][0] == '`':
-		#get section of row in between ` ` to get titles
-		temp.append(section[i][1:str(section[i][1:]).find('`')+1])
-		i += 1
-	return temp
+    i = 0 
+    temp = []
+    while section[i][0] == '`':
+        #get section of row in between ` ` to get titles
+        temp.append(section[i][1:str(section[i][1:]).find('`')+1])
+        i += 1
+    return temp
 
 def printTitles(outputFile, lst):
-	for i in lst:
-		#print in csv
-		outputFile.write("'{}',".format(i))
-	outputFile.write('\n\n')
+    for i in lst:
+        #print in csv
+        outputFile.write("'{}',".format(i))
+    outputFile.write('\n\n')
 
 def printData(section):
-	for line in section:
-		if line.startswith("INSERT INTO"):
-			substr = line
-			while len(substr) != 1:
-				csvFile.write(substr[substr.find('(')+1:substr.find(')')])
+    for line in section:
+        if line.startswith("INSERT INTO"):
+            substr = line
+            while len(substr) != 1:
+                csvFile.write(substr[substr.find('(')+1:substr.find(')')])
+#creating a new csv file from the table names 
+def createCsvFile(fileName):
+    return open(fileName+'.csv','w')
 
+
+#main open
 sqlFile = open('demo.sql', 'r')
-csvFile = open('csvfile.txt', 'w')
+#sqlFile = open('/c/Users/leonm/Desktop/python/Hw/tar2/SQL-to-CSV-converter/demo.sql','r')
+#csvFile = open('/c/Users/leonm/Desktop/python/Hw/tar2/SQL-to-CSV-converter/csvfile.txt', 'w')
 content = sqlFile.read()
+csvFile = open('csvfile.txt', 'w')
 sqlFile.close()
+tables = content.split("CREATE TABLE") #spliting the content of the file by the tables with or without their values
 
-sqlWords = content.split("CREATE TABLE")
-counter = 0
-
-filtered = list(filter(insertIntoExists, sqlWords))[1].split('\n')
-filtered2 = list(filter(insertIntoExists, sqlWords))[1].replace(" ", "").split('\n')
-printTitles(csvFile, getTitles(filtered2))
+filtered2 = list(filter(insertIntoExists, tables))[1].replace(" ", "").split('\n') #removes unnecesry tables without conetnt 
+titles=getTitles(filtered2) #the name is located in the first tile and the table titles in the rest
+csvForTest=createCsvFile(titles[0])
+printTitles(csvForTest,titles[1:])
+#main close
+csvFile.close()
